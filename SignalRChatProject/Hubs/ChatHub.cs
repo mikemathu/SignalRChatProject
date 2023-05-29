71c08aa3ef96af62fedf9822748932d888cb2d5e
Michael Mathu <mathumichael17@gmail.com>
2023-05-29 20:58:43 +0300

    Add project files.

---
+++ SignalRChatProject/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChatProject.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)// this method can be called by a connected client to send a message to all clients.
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
